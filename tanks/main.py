a3eb19fd2f514c6e62a3579e247a062a2a899458
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-25 21:33:19 -0500

    initial tank load

---
+++ tanks/main.py
@@ -0,0 +1,37 @@
+from pgzrun import go
+
+
+WIDTH = 800
+HEIGHT = 600
+
+player1 = Actor('church')
+p1_gun = Actor('church_gun')
+
+player2 = Actor('lee')
+p2_gun = Actor('lee_gun')
+
+def draw():
+  player1.draw()
+  p1_gun.draw()
+  player2.draw()
+  p2_gun.draw()
+  
+def update():
+  screen.fill('green')
+  p1_gun.pos = player1.pos
+  p1_gun.angle = player1.angle
+  if keyboard.w:
+    player1.angle = 0
+    player1.y -= 4
+  if keyboard.a:
+    player1.angle = 90
+    player1.x -= 4
+  if keyboard.s:
+    player1.angle = 180
+    player1.y += 4
+  if keyboard.d:
+    player1.angle = -90
+    player1.x += 4
+
+
+go()
